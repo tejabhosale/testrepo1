b5ca93bd47bda2d0d6f32e3b9240b60a463fe34c
tejabhosale <86197855+tejabhosale@users.noreply.github.com>
2022-02-10 22:04:59 +0530

    Create test.java

---
+++ test.java
@@ -0,0 +1,18 @@
+import java.util,*;
+
+public class animal {
+
+int num;
+String str;
+
+public void legs(int num) {
+     System.out.println("Number of legs : " + num);
+}
+
+public static void main(String[] args) {
+   
+   legs(3);
+   
+}
+
+}
